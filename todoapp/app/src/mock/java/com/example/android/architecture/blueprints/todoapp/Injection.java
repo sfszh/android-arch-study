9c1a3542a09378e654a04043923aef0008dfe217
Rui Zhang <rui.zhang@vista.co>
2017-12-09 19:18:10 +1300

    use kapt instead of annotationProcesser

--- todoapp/app/src/mock/java/com/example/android/architecture/blueprints/todoapp/Injection.java
+++ todoapp/app/src/mock/java/com/example/android/architecture/blueprints/todoapp/Injection.java
@@ -46,7 +46,7 @@ public class Injection {
 
     public static TasksRepository provideTasksRepository(@NonNull Context context) {
         checkNotNull(context);
-        ToDoDatabase database = ToDoDatabase.getInstance(context);
+        ToDoDatabase database = ToDoDatabase.Companion.getInstance(context);
         return TasksRepository.getInstance(FakeTasksRemoteDataSource.getInstance(),
                 TasksLocalDataSource.getInstance(new AppExecutors(),
                         database.taskDao()));
